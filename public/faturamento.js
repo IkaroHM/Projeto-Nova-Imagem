3f6185a184568012c45f2735f64ff91b91972a54
Ikaro Henrique Martins <ikaroo.hm@gmail.com>
2026-05-02 00:08:46 -0300

    mudancas no faturamento.js

--- public/faturamento.js
+++ public/faturamento.js
@@ -1,58 +1,8 @@
-function listarClientesFaturamento() {
-  fetch("/faturamento")
-    .then((res) => res.json())
-    .then((clientesFaturamento) => {
-        const dataCompleta = clientesFaturamento.data_completa
-        const dataObj = new Date(dataCompleta)
-        const data = dataObj.toLocaleDateString("pt-BR", { day: '2-digit', timeZone: 'America/Sao_Paulo' })
-        const html = clientesFaturamento.map(
-          (cliente) => `
-            <div class="cliente">
-              <div class="clienteInfo">
-                <span> ${cliente.nome}.  </span>
-                <span> R$:${cliente.valor}. </span>
-                <span> Dia: ${data}. </span>
-                <button class="btnApagar" onclick="apagarCliente(${cliente.id})">Apagar</button>
-              </div>
-            </div>
-        `,
-        )
-        .join("");
-        if (clientesFaturamento.length > 0) {
-          document.getElementById("listaClientesFaturamento").style.display = "block"
-        } else {
-          document.getElementById("listaClientesFaturamento").style.display = "none"
-        }
-      document.getElementById("listaClientesFaturamento").innerHTML = html;
-      document.getElementById("qntClientesMes").innerHTML = `Clientes do mes: ${clientesFaturamento.length}`
-    })
-}
+async function listarClientesFaturamento () {
+  const response = await fetch("/faturamento")
+  const clientes = await response.json()
 
-const data = new Date().toLocaleDateString("pt-BR")
-document.getElementById("mes").innerHTML = data
-
-function precoMes() {
-  fetch("/faturamento")
-    .then((res) => res.json())
-    .then((clientesFaturamento) => {
-        let totalMes = 0
-        for (const cliente of clientesFaturamento) {
-            if (cliente) {
-                totalMes = totalMes + Number(cliente.valor)
-            }
-        }
-        const html = `faturamento do mes: R$:${totalMes}`
-        document.getElementById("faturamento").innerHTML = html
-    })
-}
-
-function atualizarClientes() {
-  fetch("/faturamento", {
-    method: "DELETE"
-  }).then(() => {
-    listarClientesFaturamento()
-    precoMes()
-  })
+  console.log(clientes)
 }
 
 function apagarCliente(id) {
@@ -60,8 +10,5 @@ function apagarCliente(id) {
     method: "DELETE",
   }).then(() => {
     listarClientesFaturamento();
-    precoMes()
   });
 }
-
-atualizarClientes()
