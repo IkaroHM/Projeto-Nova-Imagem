2b856d2f0e8ce9f375ae802e9d2ff18d1fb1b675
Ikaro Henrique Martins <ikaroo.hm@gmail.com>
2026-05-04 15:46:13 -0300

    Logica de caixinhas feita no faturamento.js

--- public/faturamento.js
+++ public/faturamento.js
@@ -2,7 +2,31 @@ async function listarClientesFaturamento () {
   const response = await fetch("/faturamento")
   const clientes = await response.json()
 
-  console.log(clientes)
+  const meses ={ 
+    "janeiro": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "fevereiro": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "março": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "abril": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "maio": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "junho": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "julho": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "agosto": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "setembro": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "outubro": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "novembro": {totalClientes: 0, faturamento: 0,  clientes: [] },
+    "dezembro": {totalClientes: 0, faturamento: 0,  clientes: [] }
+  }
+
+  clientes.forEach(cliente => {
+    const nomeMes = new Date(cliente.data_completa).toLocaleString('pt-BR', { month: 'long' })
+
+    meses[nomeMes].totalClientes++
+    meses[nomeMes].faturamento += Number(cliente.valor)
+    meses[nomeMes].clientes.push(cliente)
+    
+  })
+
+  console.log(meses)
 }
 
 function apagarCliente(id) {
